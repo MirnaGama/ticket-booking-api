08eb486b62db8fdcf8420afd9a48de8f1bdc597e
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-05-21 21:10:49 -0300

    Autogenerate - Spring Inicializr

---
+++ src/main/java/com/mirna/ticketbookingapi/TicketBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.mirna.ticketbookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketbookingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketbookingapiApplication.class, args);
+	}
+
+}

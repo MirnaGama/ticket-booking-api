d3e0bcfb89f1dd2958d1de4bbe489e7d88997b51
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-05-25 01:09:24 -0300

    Add controllers for Movie and User

---
+++ src/main/java/com/mirna/ticketbookingapi/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.mirna.ticketbookingapi.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mirna.ticketbookingapi.model.User;
+import com.mirna.ticketbookingapi.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+    private UserService userService;
+
+	@GetMapping()
+    public List<User> getUsers() {
+        List<User> response = userService.findAll();
+
+        return response;
+    }
+
+	@GetMapping("{id}")
+    public User getUser(@PathVariable("id") Long id) {
+        return userService.fetchUser(id);
+    }
+
+	@PostMapping()
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PutMapping("{id}")
+    public User updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+}
